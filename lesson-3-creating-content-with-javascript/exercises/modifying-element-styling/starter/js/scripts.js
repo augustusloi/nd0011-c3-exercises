55bf59d610189136927b1948985ba30350355a76
Augustus Loi <me@augustusloi.com>
2025-09-06 16:27:17 -0400

    Centre image card

--- lesson-3-creating-content-with-javascript/exercises/modifying-element-styling/starter/js/scripts.js
+++ lesson-3-creating-content-with-javascript/exercises/modifying-element-styling/starter/js/scripts.js
@@ -2,6 +2,9 @@ const restyleOtter = async() => {
     const titleDiv = document.querySelector('body').firstElementChild;
     console.log(titleDiv);
     titleDiv.classList.add('title');
+    
+    const cardContainer = titleDiv.nextElementSibling;
+    cardContainer.id = 'cardContainer';
 }
 
 restyleOtter();
