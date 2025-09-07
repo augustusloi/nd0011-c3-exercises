1a1ab85d0cf68ee8c0f5945daca0ffa2cfaa3d9d
Augustus Loi <me@augustusloi.com>
2025-09-07 13:59:36 -0400

    Assign cardSection id

--- lesson-3-creating-content-with-javascript/exercises/adding-content-to-the-DOM/starter/js/scripts.js
+++ lesson-3-creating-content-with-javascript/exercises/adding-content-to-the-DOM/starter/js/scripts.js
@@ -1,6 +1,9 @@
 const rebuildOtter = async() => {
     const titleDiv = document.querySelector('body').firstElementChild;
     titleDiv.classList.add('title');
+
+    const cardSection = document.querySelector('section');
+    cardSection.id = 'cardContainer';
 }
 
 rebuildOtter();
